38d9a20bbed2c9c3a9023e90162276c532b9291c
Hanif <hnfsrj@gmail.com>
2024-03-14 01:25:38 +0300

    Add lazy loading to the main page

--- access/static/js/index.js
+++ access/static/js/index.js
@@ -1,2 +1,65 @@
 'use strict';
 
+let bookContainer = document.querySelector('.book_container');
+
+
+let host = 'http://127.0.0.1:8000/'
+let request = new Request(host,{
+    method:'POST',
+});
+
+
+
+let observer = new IntersectionObserver(entries=>{
+    let entry = entries[0];
+
+    if(entry.isIntersecting){
+        fetch(request)
+        .then(response=>{
+            return response.json();
+        }).then(data=>{
+            // console.log('adding');
+
+            let totalHtml = '';
+
+            data['data'].forEach((book)=>{
+                
+                let html = `
+                    <a class = link href="/book/info/${book.id}">
+                        <div class="book">
+                            <div class="book_left">
+                                <img src="${book.image}">
+                            </div>
+
+                            <div class="book_right">
+                                <p class="title">${book.title}</p>
+                                <p class="author">${book.author}</p>
+                                <p class="genre">${book.genre}</p>
+                                <p class="rating">${book.rating}/10</p>
+                            </div>
+                        </div>
+                    </a>
+                `;
+            
+                totalHtml = totalHtml + html;
+                
+            });
+            
+            bookContainer.insertAdjacentHTML('beforeend', totalHtml);
+            observer.observe(bookContainer.lastElementChild)
+
+
+        })
+        .catch(error=>{
+            console.error(error);
+        })
+
+        observer.unobserve(entry.target);
+    }
+
+},{
+    root:null,
+    rootMargin:"150px",
+});
+
+observer.observe(bookContainer.lastElementChild);
--- access/views.py
+++ access/views.py
@@ -2,18 +2,62 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from book.models import Book
 
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
 # Create your views here.
 
+@csrf_exempt
 def index(request):
-    books = Book.objects.all()[:5]
-    # print(str(books[0].image).split("/")[-1])
+    try:
+        
+        if request.method == "GET":
+            
+                books = Book.objects.all().order_by("-id")[:15]
+
+                request.session['bnum'] = 15
+
+                #this is temporary, this will change after the add book form is completed
+                for i in books:
+                    i.image = "book/"+(str(i.image).split("/")[-1])
+
+                return render(request,"index.html", {'books':books})
+            
+
+        elif request.method == "POST":
+            bnum = request.session.get("bnum",0)
+            query = Book.objects.all().order_by("-id")
+            buffer = 15
 
 
-    #this is temporary, this will change after the add book form is completed
-    for i in books:
-        i.image = "book/"+(str(i.image).split("/")[-1])
+            if bnum+buffer > len(query):
+                books = query[bnum:]
+                bnum = -buffer
+            
+            else:
+                books = query[bnum:bnum+buffer]
 
-    return render(request,"index.html", {'books':books})
+            request.session['bnum'] = bnum+buffer
+
+            data = {'data':[]}
+
+            for book in books:
+                data['data'].append({
+                    "id":book.id,
+                    "title":book.title,
+                    "author":book.author,
+                    "genre":book.genre,
+                    "rating":book.rating,
+                    "image":"static/book/"+(str(book.image).split("/")[-1]),
+                })
+
+            return JsonResponse(data)
+
+        else:
+            return HttpResponse("method not supported")
+
+    except Exception as e:
+            return HttpResponse(e)
 
 
 def register(request):
